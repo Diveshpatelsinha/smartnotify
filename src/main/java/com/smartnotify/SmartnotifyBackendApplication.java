ea1a632d48cf91c96e95ba5f62aa3019cb5bc946
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-07-17 10:30:25 +0530

    chore: initialize Spring Boot project with core dependencies (Web, Security, JPA, MySQL, WebSocket, Lombok, MapStruct, JWT, Swagger)

---
+++ src/main/java/com/smartnotify/SmartnotifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnotifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartnotifyBackendApplication.class, args);
+	}
+
+}
